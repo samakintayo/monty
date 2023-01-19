a97b4340a53ff6c82d7376a5b61077b9d151c485
Samkintayo <tayoakinnayajo@gmail.com>
2023-01-19 22:01:28 +0000

    fix

---
+++ is_number.c
@@ -0,0 +1,22 @@
+#include "monty.h"
+/**
+ * is_number - checks if the push command has a number
+ * @commands: character to be checked
+ * Return: 0 on failure, 1 on success
+ */
+int is_number(char *commands)
+{
+	if (!commands)
+		return (0);
+
+	if (*commands == 45)
+		commands++;
+
+	while (*commands != '\0')
+	{
+		if (!isdigit(*commands))
+			return (0);
+		commands++;
+												}
+	return (1);
+}
--- monty.h
+++ monty.h
@@ -1,12 +1,20 @@
-#ifndef MONTY_H
-#define MONTY_H
+#ifndef _MONTY_H_
+#define _MONTY_H_
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
 #include <unistd.h>
-#include <fcntl.h>
-#include <string.h>
 #include <ctype.h>
+#include <string.h>
+
+
+typedef struct glob_vars
+{
+	int glob_int;
+} glob_t;
+
+extern glob_t glob_vars;
+glob_t glob_vars;
+
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
@@ -21,22 +29,7 @@ typedef struct stack_s
 	struct stack_s *prev;
 	struct stack_s *next;
 } stack_t;
-/**
- * struct bus_s - variables -args, file, line content
- * @arg: value
- * @file: pointer to monty file
- * @content: line content
- * @lifi: flag change stack <-> queue
- * Description: carries values through the program
- */
-typedef struct bus_s
-{
-	char *arg;
-	FILE *file;
-	char *content;
-	int lifi;
-}  bus_t;
-extern bus_t bus;
+
 /**
  * struct instruction_s - opcode and its function
  * @opcode: the opcode
@@ -49,28 +42,30 @@ typedef struct instruction_s
 	char *opcode;
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
-char *_realloc(char *ptr, unsigned int old_size, unsigned int new_size);
-ssize_t getstdin(char **lineptr, int file);
-char  *clean_line(char *content);
-void f_push(stack_t **head, unsigned int number);
-void f_pall(stack_t **head, unsigned int number);
-void f_pint(stack_t **head, unsigned int number);
-int execute(char *content, stack_t **head, unsigned int counter, FILE *file);
-void free_stack(stack_t *head);
-void f_pop(stack_t **head, unsigned int counter);
-void f_swap(stack_t **head, unsigned int counter);
-void f_add(stack_t **head, unsigned int counter);
-void f_nop(stack_t **head, unsigned int counter);
-void f_sub(stack_t **head, unsigned int counter);
-void f_div(stack_t **head, unsigned int counter);
-void f_mul(stack_t **head, unsigned int counter);
-void f_mod(stack_t **head, unsigned int counter);
-void f_pchar(stack_t **head, unsigned int counter);
-void f_pstr(stack_t **head, unsigned int counter);
-void f_rotl(stack_t **head, unsigned int counter);
-void f_rotr(stack_t **head, __attribute__((unused)) unsigned int counter);
-void addnode(stack_t **head, int n);
-void addqueue(stack_t **head, int n);
-void f_queue(stack_t **head, unsigned int counter);
-void f_stack(stack_t **head, unsigned int counter);
+
+void _sfree(stack_t *args);
+void _free(stack_t **head, unsigned int n);
+char *get_line(char *file);
+char **split_line(char *line);
+void *_realloc(void *ptr, size_t old_size, size_t new_size);
+int is_number(char *commands);
+
+int checker(unsigned int line_number, char **commands, stack_t **head, FILE *fptr);
+void (*_commands(char *opcode, unsigned int ln))(stack_t **stack, unsigned int);
+
+void push(stack_t **stack, unsigned int line_number);
+void pall(stack_t **stack, unsigned int line_number);
+
+void pint(stack_t **stack, unsigned int line_number);
+void pop(stack_t **stack, unsigned int line_number);
+
+void swap(stack_t **stack, unsigned int line_number);
+void add(stack_t **stack, unsigned int line_number);
+
+void nop(stack_t **stack, unsigned int line_number);
+void sub(stack_t **stack, unsigned int line_number);
+void _div(stack_t **stack, unsigned int line_number);
+void mul(stack_t **stack, unsigned int line_number);
+void mod(stack_t **stack, unsigned int line_number);
+
 #endif
